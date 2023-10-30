0768612721586ce82ef04930c87e42f9c0157522
seansarley <ss00154@mix.wvu.edu>
2023-10-30 16:38:39 -0400

    Lab2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>CS230L</h1>
       <h2>Section - 003</h2>
       <p>WVU ID: 800396484</p>
       <p>Hi I am Sean Sarley</p>
+      <Card></Card>
     </div>
   );
 }
