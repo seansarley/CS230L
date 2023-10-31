a25b455b126c56deace1701316dcc1a79c92edb9
seansarley <ss00154@mix.wvu.edu>
2023-10-31 13:07:12 -0400

    lab2

--- src/Card.js
+++ src/Card.js
@@ -1,25 +1,29 @@
 function Card () {
     return (
-        <div class="card" style={{width: '25rem'}}>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <div>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-        <div>
-        <div class="card-body">
+      <div class = "container">
+      <div style={{display: 'flex', justifycontent: 'space-around'}}>        
+        <div class="card1" style={{width: '25rem'}}>
+        <div class="card-body" >
           <h5 class="card-title">Card title</h5>
           <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
         </div>
+        <div class="card2" style = {{width: '25rem'}}>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+        <div class ="card3" style ={{width:'25rem'}}>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+      </div>
       </div>
     )
 }
